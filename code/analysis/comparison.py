feb5dea447d030cb308ea3f7923b35be3299714f
Tyler Tucker <tylertucker1@ufl.edu>
2024-11-18 19:54:06 -0500

    Improved comparison and event scripts

--- code/analysis/comparison.py
+++ code/analysis/comparison.py
@@ -1,101 +1,92 @@
 # Import libraries
-from datetime import datetime
+from datetime import datetime, time, timedelta, date
 import matplotlib
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from pathlib import Path
+
+# matplotlib settings before plotting data
+def pre_plot_settings():
+    # Pre-plot settings
+    matplotlib.rc('font', size=16)
+    matplotlib.rc('xtick', labelsize=14)
+    matplotlib.rc('ytick', labelsize=14)
+    fig, ax = plt.subplots(figsize=(10,10))
+    plt.grid(visible=True,
+                linewidth=0.5,
+                alpha=0.5)
+
+    # Show y-axis tick marks at 10% intervals
+    yticks_ratios = np.arange(0, 1.001, 0.1)
+
+    # Format x-axis as timestamps and show tick marks at 15 minute intervals
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=15))
+
+    # More settings
+    ax.set(
+        xlabel='Time',
+        xlim=[datetime.strptime('1970-1-1 1:45:00', '%Y-%m-%d %H:%M:%S'), datetime.strptime('1970-1-1 2:15:00', '%Y-%m-%d %H:%M:%S')],
+        ylim=(0,1),
+        yticks=yticks_ratios,
+        yticklabels=['{:.0%}'.format(my_yticklabel) for my_yticklabel in yticks_ratios],
+        ylabel='Security Mode Command Ratio')
+
+# Plot data from a given dataframe
+def plot_capture(input_df, color, label, offset):
+
+    # Iterate through IMSI-exposing messages
+    my_df = input_df.loc[input_df['IMSI-Exposing Message'] != '']
+    bins = []
+    ratios = []
+    for exposure in my_df.iterrows():
+
+        # Strip date from epoch object
+        epoch = exposure[1]['Timestamp']
+        start = float(epoch - (epoch % 60))
+        cur_date = datetime.fromtimestamp(start)
+        cur_time = time(cur_date.hour, cur_date.minute, cur_date.second)
+        stripped_date = datetime.combine(date(1970, 1, 1), cur_time)
+
+        # Only process the time bin if it has not been previously seen
+        if stripped_date not in bins:
+            bins.append(stripped_date)
+            end = start + 60
+            relevant_df = input_df.loc[(input_df['Timestamp'] >= start) & (input_df['Timestamp'] < end)]
+            ratio = len(relevant_df.loc[relevant_df['IMSI-Exposing Message'] != '']) / len(relevant_df)
+            ratios.append(ratio)
+
+    # Implement offset for grouped plots
+    for i in range(0, len(bins)):
+        bins[i] += timedelta(seconds=offset)
+
+    # Scatter plot
+    plt.bar(bins,
+            ratios,
+            width=0.0002,
+            color=color,
+            edgecolor='black',
+            label=label,
+            zorder = 3)
+
+# matplotlib settings after plotting data
+def post_plot_settings():
+
+    # Show legend and save plot to local file
+    plt.legend()
+    fig_file_path = 'comparison.pdf'
+    plt.savefig(fig_file_path, bbox_inches='tight')
+    print(f'Saved file {fig_file_path}.')
 
 # Read in data
 data_directory = Path('../../data/lte')
 dlprobe_df = pd.read_pickle(data_directory / 'comparison/dlprobe.pkl')
 ltesniffer_df = pd.read_pickle(data_directory / 'comparison/ltesniffer.pkl')
 
-# Pre-plot settings
-matplotlib.rc('font', size=16)
-matplotlib.rc('xtick', labelsize=14)
-matplotlib.rc('ytick', labelsize=14)
-fig, ax = plt.subplots(figsize=(10,10))
-plt.grid(visible=True,
-            linewidth=0.5,
-            alpha=0.5)
-xticks_minutes = np.arange(105, 136, 15)
-yticks_ratios = np.arange(0, 1.001, 0.1)
-ax.set(xlim=(105, 135),
-    xticks=xticks_minutes,
-    xticklabels=[pd.to_datetime(tm, unit='m').strftime('%H:%M') for tm in xticks_minutes],
-    xlabel='Time',
-    ylim=(0,1),
-    yticks=yticks_ratios,
-    yticklabels=['{:.0%}'.format(my_yticklabel) for my_yticklabel in yticks_ratios],
-    ylabel='Security Mode Command Ratio')
-colors = ['#0021A5', '#FA4616']
-
-# Iterate through IMSI-exposing messages
-my_df = dlprobe_df.loc[dlprobe_df['IMSI-Exposing Message'] != '']
-bins = []
-ratios = []
-for exposure in my_df.iterrows():
-    epoch = exposure[1]['Timestamp']
-    start = float(epoch - (epoch % 60))
-    if start not in bins:
-        bins.append(start)
-        end = start + 60
-        relevant_df = dlprobe_df.loc[(dlprobe_df['Timestamp'] >= start) & (dlprobe_df['Timestamp'] < end)]
-        ratio = len(relevant_df.loc[relevant_df['IMSI-Exposing Message'] != '']) / len(relevant_df)
-        ratios.append(ratio)
-
-# Convert epoch bins to minute bins
-for i in range(0,len(bins)):
-    dt = datetime.fromtimestamp(bins[i])
-    bins[i] = dt.hour * 60 + dt.minute
-
-bins = bins[:int(len(bins)/2)]
-ratios = ratios[:int(len(ratios)/2)]
-
-# Scatter plot
-plt.bar(bins,
-        ratios,
-        width=0.3,
-        color=colors[0],
-        linewidth=0.3,
-        edgecolor='black',
-        label='DlProbe',
-        zorder = 3)
-
-# Iterate through IMSI-exposing messages
-my_df = ltesniffer_df.loc[ltesniffer_df['IMSI-Exposing Message'] != '']
-bins = []
-ratios = []
-for exposure in my_df.iterrows():
-    epoch = exposure[1]['Timestamp']
-    start = float(epoch - (epoch % 60))
-    if start not in bins:
-        bins.append(start)
-        end = start + 60
-        relevant_df = ltesniffer_df.loc[(ltesniffer_df['Timestamp'] >= start) & (ltesniffer_df['Timestamp'] < end)]
-        ratio = len(relevant_df.loc[relevant_df['IMSI-Exposing Message'] != '']) / len(relevant_df)
-        ratios.append(ratio)
-
-bins = bins[:int(len(bins)/2)]
-ratios = ratios[:int(len(ratios)/2)]
-
-# Convert epoch bins to minute bins
-for i in range(0,len(bins)):
-    dt = datetime.fromtimestamp(bins[i])
-    bins[i] = (dt.hour - 20) * 60 + dt.minute
-
-# Scatter plot
-plt.bar([x + 0.3 for x in bins],
-        ratios,
-        width=0.3,
-        color=colors[1],
-        linewidth=0.4,
-        edgecolor='black',
-        label='LTESniffer',
-        zorder = 3)
-
-plt.legend()
-fig_file_path = 'comparison.pdf'
-plt.savefig(fig_file_path, bbox_inches='tight')
-print(f'Saved file {fig_file_path}.')
+# Plot both captures
+pre_plot_settings()
+plot_capture(dlprobe_df, '#0021A5', 'DlProbe', -10)
+plot_capture(ltesniffer_df, '#FA4616', 'LTESniffer', 10)
+post_plot_settings()
--- code/analysis/event.py
+++ code/analysis/event.py
@@ -1,11 +1,10 @@
-# Visualize Cellular Network Captures
-
 # Import all prerequisite packages
-import pandas as pd
+from datetime import datetime, time, date
 import matplotlib
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
-from datetime import datetime
 import numpy as np
+import pandas as pd
 from pathlib import Path
 
 # Function to plot capture results on a 24 hour scale
@@ -30,25 +29,6 @@ def plot_data(connections_file, file_path):
                           'Service Reject':                 '#E76F51',
                           'TAU Reject':                     '#2A9D8F'}
 
-    # Pre-plot settings
-    matplotlib.rc("font", size=16)
-    matplotlib.rc("xtick", labelsize=14)
-    matplotlib.rc("ytick", labelsize=14)
-    fig, ax = plt.subplots(figsize=(10,2.5))
-    plt.grid(visible=True,
-             linewidth=0.5,
-             alpha=0.9)
-    xticks_minutes = np.arange(840, 961, 30)
-    yticks_ratios = np.arange(0.1, 0.51, 0.1)
-    ax.set(xlim=(840, 960),
-        xticks=xticks_minutes,
-        xticklabels=[pd.to_datetime(tm, unit='m').strftime("%H:%M") for tm in xticks_minutes],
-        xlabel='Time of Day',
-        ylim=(0,0.5),
-        yticks=yticks_ratios,
-        yticklabels=["{:.0%}".format(my_yticklabel) for my_yticklabel in yticks_ratios],
-        ylabel='IE Ratio')
-
     # Ensure file is in pcap format
     if not connections_file.endswith('.pkl'): return
 
@@ -61,29 +41,49 @@ def plot_data(connections_file, file_path):
     for exposure in my_df.iterrows():
         epoch = exposure[1]['Timestamp']
         start = float(epoch - (epoch % 60))
-        if start not in bins:
-            bins.append(start)
+        cur_date = datetime.fromtimestamp(start)
+        cur_time = time(cur_date.hour, cur_date.minute, cur_date.second)
+        stripped_date = datetime.combine(date(1970, 1, 1), cur_time)
+        if stripped_date not in bins:
+            bins.append(stripped_date)
             end = start + 60
             relevant_df = connections_df.loc[(connections_df['Timestamp'] >= start) & (connections_df['Timestamp'] < end)]
             for message in message_ratios_dict.keys():
                 message_ratios_dict[message].append(len(relevant_df.loc[relevant_df['IMSI-Exposing Message'] == message]) / len(relevant_df))
 
-    # Convert epoch bins to minute bins
-    for i in range(0,len(bins)):
-        dt = datetime.fromtimestamp(bins[i])
-        bins[i] = dt.hour * 60 + dt.minute
+    # Pre-plot settings
+    matplotlib.rc("font", size=16)
+    matplotlib.rc("xtick", labelsize=14)
+    matplotlib.rc("ytick", labelsize=14)
+    fig, ax = plt.subplots(figsize=(10,2.5))
+    plt.grid(visible=True,
+             linewidth=0.5,
+             alpha=0.9)
     
-    # Scatter plot
+    # Format x-axis as timestamps and show tick marks at 15 minute intervals
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    ax.xaxis.set_major_locator(mdates.MinuteLocator(interval=30))
+
+    # xticks_minutes = np.arange(840, 961, 30)
+    yticks_ratios = np.arange(0.1, 0.51, 0.1)
+    ax.set(
+        xlabel='Time of Day',
+        xlim=[datetime.strptime('1970-1-1 14:00:00', '%Y-%m-%d %H:%M:%S'), datetime.strptime('1970-1-1 16:00:00', '%Y-%m-%d %H:%M:%S')],
+        ylim=(0,0.5),
+        yticks=yticks_ratios,
+        yticklabels=["{:.0%}".format(my_yticklabel) for my_yticklabel in yticks_ratios],
+        ylabel='IE Ratio')
+    
+    # Bar plot
     bottom_list = [0] * len(message_ratios_dict['Identity Request'])
     for message in message_ratios_dict.keys():
         if not all(v == 0 for v in message_ratios_dict[message]):
             plt.bar(bins,
                     message_ratios_dict[message],
-                    width=1,
+                    width=0.0006,
                     label=message,
                     bottom=bottom_list,
                     color=message_color_dict[message],
-                    linewidth=0.4,
                     edgecolor="black",
                     zorder = 3)
             bottom_list = [sum(x) for x in zip(message_ratios_dict[message], bottom_list)]
